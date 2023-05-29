507a170d80b93207d90288b0e2746e536f91e877
AnviMittal <a2mittal@ucsd.edu>
2023-05-28 17:39:18 -0700

    Completed lab 8

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 8 - Starter
+Anvi Mittal 
+1. Within a GitHub action that runs whenever code is pushed. This is to incorporate automated tests into the Recipe project development pipeline. This ensures consistent quality, faster feedback, and reliable software delivery in the Recipe project development pipeline.
+2. No
+3. No because the "message" feature involves interaction between multiple components and a unit test may not be the most appropriate choice for testing.
+4. Yes, the "max message length" feature operates on a smaller scale and does not require interaction with other components of the application. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,37 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('checks for valid phone numbers', () => {
+    expect(functions.isPhoneNumber('(858)319-5642')).toBe(true);
+    expect(functions.isPhoneNumber('(858)999-4010')).toBe(true);
+    expect(functions.isPhoneNumber('(858)abc')).toBe(false);
+    expect(functions.isPhoneNumber('(858)674-abcd')).toBe(false);
+})
+
+test('checks for valid email id', () => {
+    expect(functions.isEmail('anvimittal26@gmail.com')).toBe(true);
+    expect(functions.isEmail('mittal@cse.edu')).toBe(true);
+    expect(functions.isEmail('hey.gmail.com')).toBe(false);
+    expect(functions.isEmail('hey.com')).toBe(false);
+})
+
+test('checks if password is strong', () => {
+    expect(functions.isStrongPassword('Anvimittal8')).toBe(true);
+    expect(functions.isStrongPassword('esfjndhf2635')).toBe(true);
+    expect(functions.isStrongPassword('34gfsjfhud')).toBe(false);
+    expect(functions.isStrongPassword('atfbsu@675')).toBe(false);
+})
+
+test('checks for the correct date format', () => {
+    expect(functions.isDate('05/28/2023')).toBe(true);
+    expect(functions.isDate('8/26/2003')).toBe(true);
+    expect(functions.isDate('5/05/20023')).toBe(false);
+    expect(functions.isDate('056/657/2023')).toBe(false);
+})
+
+test('checks for valid hex colour', () => {
+    expect(functions.isHexColor('FFFFFF')).toBe(true);
+    expect(functions.isHexColor('000000')).toBe(true);
+    expect(functions.isHexColor('12')).toBe(false);
+    expect(functions.isHexColor('ABCD')).toBe(false);
+})
